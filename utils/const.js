1779334b01e342726ba6eed01060005045b0ec68
JeremyNoh <jeremynohile@gmail.com>
2019-04-22 00:04:19 +0200

    Delete const.js

--- utils/const.js
+++
@@ -1,8 +0,0 @@
-export const OPTIONS_AWS = {
-  keyPrefix: "quickpic/",
-  bucket: "quickpic-20190421160607-deployment",
-  region: "us-east-1",
-  accessKey: "AKIAJGHACTPBATOQX3WA",
-  secretKey: "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",
-  successActionStatus: 201
-};
